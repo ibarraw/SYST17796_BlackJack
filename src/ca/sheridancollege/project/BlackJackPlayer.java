e4efc2756e5db6019e6915832e563a038fe2ac1f
WXJ <WXJ@LAPTOP-6QI6U7UJ>
2021-07-17 19:36:15 -0400

    Copy base code to this Game Class. Create BlackJackDealer and BlackJackPlayer Classes.

---
+++ src/ca/sheridancollege/project/BlackJackPlayer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.sheridancollege.project;
+
+/**
+ * This class used to hold Black Jack Card game
+ *
+ * @author Xianjun Wang July 2021
+ */
+public class BlackJackPlayer {
+
+}
--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -13,7 +13,7 @@ package ca.sheridancollege.project;
  *
  * @author dancye
  * @author William Ibarra
- * @modifier Xianjun Wang  June 2021
+ * @modifier Xianjun Wang  July 2021
  */
 public abstract class Card { //default modifier for child classes
 
